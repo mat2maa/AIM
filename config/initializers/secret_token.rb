a8e9b95388303ac7f977aa7581754746c6a43ff5
Matthew Ager <matthew.a.ager@googlemail.com>
2013-07-10 11:21:50 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aim::Application.config.secret_token = '5d4f4c22fdaa6f2d429170473f2f0502d699c6a451b558910657696335aa945ee6d49ea48b77625fe8a288336e32498b7dc926c3a83ecb07eb5ea1ea247dad73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Aim::Application.config.session_store :cookie_store, key: '_aim_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Aim::Application.config.session_store :active_record_store
