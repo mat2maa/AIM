728ee6ef7abeba606e9e287105eaeee8f735b59e
Matthew Ager <matthew.a.ager@googlemail.com>
2013-08-21 02:17:06 +0200

    Added storyboard to assets path

--- config/application.rb
+++ config/application.rb
@@ -81,5 +81,7 @@ module Aim
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+
+    config.assets.paths << "#{Rails.root}/app/assets/images/storyboard"
   end
 end
